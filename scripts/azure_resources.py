cce0b3e0ec51c7846f69ef75a2028c26eabca2ab
Vibhore <vibhore.miglani@inmobi.com>
2021-05-03 00:05:30 +0530

    mutliple fixes & indentation

--- scripts/azure_resources.py
+++ scripts/azure_resources.py
@@ -1,271 +1,265 @@
-def azure_resources(crf,cde,crg,headers,requests,sub,json,az2tfmess,os,cldurl):
+def azure_resources(crf, cde, crg, headers, requests, sub, json, az2tfmess, os, cldurl):
     # print "REST Resources ",
-    fresfilename="data.json"
-    fres=open(fresfilename, 'w')
-    url="https://" + cldurl + "/subscriptions/" + sub + "/resources"
+    fresfilename = "data.json"
+    fres = open(fresfilename, 'w')
+    url = "https://" + cldurl + "/subscriptions/" + sub + "/resources"
     params = {'api-version': '2018-11-01'}
-    try: 
+    try:
         r = requests.get(url, headers=headers, params=params)
-        res= r.json()["value"]
+        res = r.json()["value"]
     except KeyError:
-        print ("Error getting resources")
+        print("Error getting resources")
         exit()
     fres.write(json.dumps(res, indent=4, separators=(',', ': ')))
     fres.close()
 
+    rfilename = "resources2.txt"
+    fr = open(rfilename, 'w')
+    nprfilename = "noprovider2.txt"
+    np = open(nprfilename, 'w')
 
-    rfilename="resources2.txt"
-    fr=open(rfilename, 'w')
-    nprfilename="noprovider2.txt"
-    np=open(nprfilename, 'w')
-
-
-    count=len(res)
-    print ("Resources Found: " + str(count))
+    count = len(res)
+    print("Resources Found: " + str(count))
     for j in range(0, count):
-        
-        #name=res[j]['name']
-        id=res[j]['id']
-        rg1=id.split("/")[4]
-        try:
-            isext=id.split("/")[9]
-        except IndexError:
-            isext=""
 
-        rtype=res[j]['type']
-        rg=rg1.replace(".","-")
-        #print rtype
+        # name=res[j]['name']
+        id = res[j]['id']
+        rg1 = id.split("/")[4]
+        try:
+            isext = id.split("/")[9]
+        except IndexError:
+            isext = ""
+
+        rtype = res[j]['type']
+        rg = rg1.replace(".", "-")
+        # print rtype
 
         if rtype == "Microsoft.Compute/availabilitySets":
-            prov="azurerm_availability_set"
+            prov = "azurerm_availability_set"
             fr.write(rg + ":" + prov + "\n")
         elif rtype == "Microsoft.Network/networkSecurityGroups":
-            prov="azurerm_network_security_group"
+            prov = "azurerm_network_security_group"
             fr.write(rg + ":" + prov + "\n")
 
-        elif rtype == "Microsoft.Storage/storageAccounts": 
-            prov="azurerm_storage_account"
+        elif rtype == "Microsoft.Storage/storageAccounts":
+            prov = "azurerm_storage_account"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_storage_share"
+            prov = "azurerm_storage_share"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_storage_container"
+            prov = "azurerm_storage_container"
             fr.write(rg + ":" + prov + "\n")
 
         elif rtype == "Microsoft.Network/networkSecurityGroups":
-            prov="azurerm_network_security_group"
+            prov = "azurerm_network_security_group"
             fr.write(rg + ":" + prov + "\n")
 
-        elif rtype == "Microsoft.Compute/virtualMachines": 
-            #echo $isext
+        elif rtype == "Microsoft.Compute/virtualMachines":
+            # echo $isext
             if isext != "extensions":
-                prov="azurerm_virtual_machine"
+                prov = "azurerm_virtual_machine"
                 fr.write(rg + ":" + prov + "\n")
-                    
-        elif rtype == "Microsoft.Network/networkInterfaces": 
-            prov="azurerm_network_interface"
+
+        elif rtype == "Microsoft.Network/networkInterfaces":
+            prov = "azurerm_network_interface"
             fr.write(rg + ":" + prov + "\n")
-        
+
         elif rtype == "Microsoft.Compute/disks":
-            prov="azurerm_managed_disk"
+            prov = "azurerm_managed_disk"
             fr.write(rg + ":" + prov + "\n")
-            
-        elif rtype == "Microsoft.Automation/automationAccounts": 
-            prov="azurerm_automation_account"
+
+        elif rtype == "Microsoft.Automation/automationAccounts":
+            prov = "azurerm_automation_account"
             fr.write(rg + ":" + prov + "\n")
-            
+
         elif rtype == "Microsoft.Network/virtualNetworks":
-            prov="azurerm_virtual_network"
+            prov = "azurerm_virtual_network"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_subnet"
+            prov = "azurerm_subnet"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_virtual_network_peering"
+            prov = "azurerm_virtual_network_peering"
             fr.write(rg + ":" + prov + "\n")
 
         elif rtype == "Microsoft.Network/publicIPAddresses":
-            prov="azurerm_public_ip"
+            prov = "azurerm_public_ip"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/loadBalancers":
-            prov="azurerm_lb"
+            prov = "azurerm_lb"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_lb_nat_rule"
+            prov = "azurerm_lb_nat_rule"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_lb_nat_pool"
+            prov = "azurerm_lb_nat_pool"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_lb_backend_address_pool"
+            prov = "azurerm_lb_backend_address_pool"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_lb_probe"
+            prov = "azurerm_lb_probe"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_lb_rule"
+            prov = "azurerm_lb_rule"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/virtualNetworkGateways":
-            prov="azurerm_virtual_network_gateway"
+            prov = "azurerm_virtual_network_gateway"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/connections":
-            prov="azurerm_virtual_network_gateway_connection"
+            prov = "azurerm_virtual_network_gateway_connection"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Network/routeTables": 
-            prov="azurerm_route_table"
+
+        elif rtype == "Microsoft.Network/routeTables":
+            prov = "azurerm_route_table"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.OperationalInsights/workspaces":
-            prov="azurerm_log_analytics_workspace"
+            prov = "azurerm_log_analytics_workspace"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype ==  "Microsoft.OperationsManagement/solutions":
-            prov="azurerm_log_analytics_solution"
+
+        elif rtype == "Microsoft.OperationsManagement/solutions":
+            prov = "azurerm_log_analytics_solution"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.KeyVault/vaults":
-            prov="azurerm_key_vault"
+            prov = "azurerm_key_vault"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_key_vault_secret"
+            prov = "azurerm_key_vault_secret"
             fr.write(rg + ":" + prov + "\n")
 
         elif rtype == "Microsoft.RecoveryServices/vaults":
-            prov="azurerm_recovery_services_vault"
+            prov = "azurerm_recovery_services_vault"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.ContainerRegistry/registries":
-            prov="azurerm_container_registry"
+            prov = "azurerm_container_registry"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.ContainerService/managedClusters":
-            prov="azurerm_kubernetes_cluster"
+            prov = "azurerm_kubernetes_cluster"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/localNetworkGateways":
-            prov="azurerm_local_network_gateway"
+            prov = "azurerm_local_network_gateway"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/expressRouteCircuits":
-            prov="azurerm_express_route_circuit"
+            prov = "azurerm_express_route_circuit"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_express_route_circuit_authorization"
+            prov = "azurerm_express_route_circuit_authorization"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_express_route_circuit_peering"
+            prov = "azurerm_express_route_circuit_peering"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Compute/images": 
-            prov="azurerm_image"
+
+        elif rtype == "Microsoft.Compute/images":
+            prov = "azurerm_image"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Network/networkWatchers": 
-            prov="azurerm_network_watcher"
+
+        elif rtype == "Microsoft.Network/networkWatchers":
+            prov = "azurerm_network_watcher"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Network/applicationSecurityGroups":
-            prov="azurerm_application_security_group"
+            prov = "azurerm_application_security_group"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.ContainerInstance/containerGroups":
-            prov="azurerm_container_group"
+            prov = "azurerm_container_group"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Network/applicationGateways": 
-            prov="azurerm_application_gateway"
+
+        elif rtype == "Microsoft.Network/applicationGateways":
+            prov = "azurerm_application_gateway"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.DocumentDb/databaseAccounts":
-            prov="azurerm_cosmosdb_account"
+            prov = "azurerm_cosmosdb_account"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.ServiceBus/namespaces": 
-            prov="azurerm_servicebus_namespace"
+
+        elif rtype == "Microsoft.ServiceBus/namespaces":
+            prov = "azurerm_servicebus_namespace"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_servicebus_queue"
+            prov = "azurerm_servicebus_queue"
             fr.write(rg + ":" + prov + "\n")
-                    
+
         elif rtype == "Microsoft.Network/trafficmanagerprofiles":
-            prov="azurerm_traffic_manager_profile"
+            prov = "azurerm_traffic_manager_profile"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_traffic_manager_endpoint"
+            prov = "azurerm_traffic_manager_endpoint"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Web/serverFarms": 
-            prov="azurerm_app_service_plan"
+
+        elif rtype == "Microsoft.Web/serverFarms":
+            prov = "azurerm_app_service_plan"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Web/sites": 
-            prov="azurerm_app_service"
+
+        elif rtype == "Microsoft.Web/sites":
+            prov = "azurerm_app_service"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_function_app"
+            prov = "azurerm_function_app"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Compute/virtualMachineScaleSets":
-            prov="azurerm_virtual_machine_scale_set"
+            prov = "azurerm_virtual_machine_scale_set"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.ManagedIdentity/userAssignedIdentities":
-            prov="azurerm_user_assigned_identity"
+            prov = "azurerm_user_assigned_identity"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Compute/snapshots":
-            prov="azurerm_snapshot"
+            prov = "azurerm_snapshot"
             fr.write(rg + ":" + prov + "\n")
-                
+
         elif rtype == "Microsoft.Databricks/workspaces":
-            prov="azurerm_databricks_workspace"
+            prov = "azurerm_databricks_workspace"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Sql/servers": 
-            prov="azurerm_sql_server"
+
+        elif rtype == "Microsoft.Sql/servers":
+            prov = "azurerm_sql_server"
             fr.write(rg + ":" + prov + "\n")
-            prov="azurerm_sql_database"
+            prov = "azurerm_sql_database"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype == "Microsoft.Network/dnszones": 
-            prov="azurerm_dns_zone"
+
+        elif rtype == "Microsoft.Network/dnszones":
+            prov = "azurerm_dns_zone"
             fr.write(rg + ":" + prov + "\n")
-                
-        elif rtype ==  "microsoft.insights/autoscalesettings":
-            prov="azurerm_monitor_autoscale_setting"
+
+        elif rtype == "microsoft.insights/autoscalesettings":
+            prov = "azurerm_monitor_autoscale_setting"
             fr.write(rg + ":" + prov + "\n")
-                
+
         else:
             np.write(rtype + "\n")
 
     fr.close()
     np.close()
 
-    print ("Optimizing Resources ...")
+    print("Optimizing Resources ...")
     # sort unique and filter for Resource Group
-    rfilename="resources.txt"
-    fr=open(rfilename, 'w')
+    rfilename = "resources.txt"
+    fr = open(rfilename, 'w')
     with open('resources2.txt', 'r') as r:
         for line in sorted(set(r)):
-            trg=line.split(":")[0]
-            trt=line.split(":")[1]
-            #print trt
-            if crg is not None:   # Resource Group Filter
-                if trg == crg :
-                    if crf is not None:   # Resource Filter
+            trg = line.split(":")[0]
+            trt = line.split(":")[1]
+            # print trt
+            if crg is not None:  # Resource Group Filter
+                if trg == crg:
+                    if crf is not None:  # Resource Filter
                         if crf in trt:
-                            fr.write(line,)
+                            fr.write(line, )
                     else:
-                        fr.write(line,)
+                        fr.write(line, )
             else:
-                if crf is not None:   # Resource Filter
-                    if crf in trt :
-                        fr.write(line,)
+                if crf is not None:  # Resource Filter
+                    if crf in trt:
+                        fr.write(line, )
                 else:
-                    fr.write(line,)
+                    fr.write(line, )
     r.close()
     fr.close()
 
-
-
-
-
     # sort unique and fileter for Resource Group
-    rfilename="noprovider.txt"
-    fr=open(rfilename, 'w')
+    rfilename = "noprovider.txt"
+    fr = open(rfilename, 'w')
     with open('noprovider2.txt', 'r') as r:
         for line in sorted(set(r)):
-            fr.write(line,)
+            fr.write(line, )
 
     r.close()
     fr.close()
--- scripts/azurerm_resource_group.py
+++ scripts/azurerm_resource_group.py
@@ -1,87 +1,101 @@
 import sys
-def azurerm_resource_group(crf,cde,crg,headers,requests,sub,json,az2tfmess,cldurl):
+
+from utils.utils import get_tf_state_rm_file_path, get_tf_import_state_script_path
+
+
+def azurerm_resource_group(crf, cde, filter_rg, headers, requests, sub, json, az2tfmess, cldurl):
     # handle resource groups
-    isrun=False
-    tfp="azurerm_resource_group"
+    """
+    :param crf: resource filter
+    :param cde: debug
+    :param filter_rg: resource group
+    :param headers: request headers
+    :param requests: requests library object :(
+    :param sub: subscription
+    :param json: json library object :(
+    :param az2tfmess: msg for file
+    :param cldurl: cloud domain
+    :return:
+    """
+    isrun = False
+    tfp = "azurerm_resource_group"
     if crf in tfp:
-        
-        print('# ' + tfp,)
-  
-        tfrmf="001-"+tfp+"-staterm.sh"
-        tfimf="001-"+tfp+"-stateimp.sh"
-        tfrm=open(tfrmf, 'a')
-        tfim=open(tfimf, 'a')
-        url="https://" + cldurl + "/subscriptions/" + sub + "/resourceGroups"
+
+        print('# ' + tfp, )
+
+        # fetch all resource groups
+        url = "https://" + cldurl + "/subscriptions/" + sub + "/resourceGroups"
         params = {'api-version': '2014-04-01'}
         r = requests.get(url, headers=headers, params=params)
-        rgs= r.json()["value"]
+        rgs = r.json()["value"]
 
-        #frgfilename=tfp+".json"
-        #frg=open(frgfilename, 'w')
-        #frg.write(json.dumps(rgs, indent=4, separators=(',', ': ')))
-        #frg.close()
         if cde:
             print(json.dumps(rgs, indent=4, separators=(',', ': ')))
 
-
-
-        count=len(rgs)
-        print (count)
+        count = len(rgs)
+        print(count)
         for j in range(0, count):
-            
-            name=rgs[j]["name"]
-            rg=name
-            loc=rgs[j]["location"]
-            id=rgs[j]["id"]
-            if crg is not None:
-                if name.lower() != crg.lower():
+            name = rgs[j]["name"]
+            rg = name
+            loc = rgs[j]["location"]
+            id = rgs[j]["id"]
+
+            tf_rm_path = get_tf_state_rm_file_path(resource_name=tfp, resource_group=rg)
+            tf_imp_path = get_tf_import_state_script_path(resource_name=tfp, resource_group=rg)
+            tf_rm = open(tf_rm_path, 'a')
+            tf_imp = open(tf_imp_path, 'a')
+
+            if filter_rg is not None:
+                if name.lower() != filter_rg.lower():
                     continue
-            
-            rname=name.replace(".","-")
-            if rg[0].isdigit(): rg="rg_"+rg
-                
-            if rname[0].isdigit(): rname="rg_"+rname
-            prefix=tfp+"."+rname
-            
-            rfilename=prefix+".tf"
+
+            rname = name.replace(".", "-")
+            if rg[0].isdigit():
+                rg = "rg_" + rg
+
+            if rname[0].isdigit():
+                rname = "rg_" + rname
+            prefix = tfp + "." + rname
+
+            rfilename = prefix + ".tf"
             if isrun:
-                fr=sys.stdout
+                fr = sys.stdout
             else:
-                fr=open(rfilename, 'w')
+                fr = open(rfilename, 'w')
             fr.write("")
             fr.write('resource "' + tfp + '" "' + rname + '" {\n')
             fr.write('\t name = "' + name + '"\n')
-            fr.write('\t location = "'+ loc + '"\n')
-        
+            fr.write('\t location = "' + loc + '"\n')
 
-        # tags block
+            # tags block
             try:
-                mtags=rgs[j]["tags"]
+                mtags = rgs[j]["tags"]
             except:
-                mtags="{}"
-            tcount=len(mtags)-1
-            if tcount > 1 :
+                mtags = "{}"
+            tcount = len(mtags) - 1
+            if tcount > 1:
                 fr.write('tags = { \n')
-                #print tcount
+                # print tcount
                 for key in mtags.keys():
-                    tval=mtags[key]
+                    tval = mtags[key]
                     fr.write(('\t "' + key + '"="' + tval + '"\n'))
-                #print(json.dumps(mtags, indent=4, separators=(',', ': ')))
+                # print(json.dumps(mtags, indent=4, separators=(',', ': ')))
                 fr.write('}\n')
-            
-            fr.write('}\n') 
-            if fr is not sys.stdout: fr.close()  # close .tf file
+
+            fr.write('}\n')
+            if fr is not sys.stdout:
+                fr.close()  # close .tf file
 
             if cde:
-                with open(rfilename) as f: 
-                    print (f.read())
+                with open(rfilename) as f:
+                    print(f.read())
 
-            tfrm.write('terraform state rm '+tfp+'.'+rname + '\n')
-            tfim.write('echo "importing ' + str(j) + ' of ' + str(count-1) + '"' + '\n')
-            tfcomm='terraform import '+tfp+'.'+rname+' '+id+'\n'
-            tfim.write(tfcomm)
+            tf_rm.write('terraform state rm ' + tfp + '.' + rname + '\n')
+            tf_imp.write('echo "importing ' + str(j) + ' of ' + str(count - 1) + '"' + '\n')
+            tfcomm = 'terraform import ' + tfp + '.' + rname + ' ' + id + '\n'
+            tf_imp.write(tfcomm)
 
         # end for
-        tfrm.close()
-        tfim.close()
-        #end resource group
+        tf_rm.close()
+        tf_imp.close()
+        # end resource group

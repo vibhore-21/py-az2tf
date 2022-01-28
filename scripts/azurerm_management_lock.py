36004f9846ebfcad2fd3cd25c427ddd4e7cdade7
Vibhore <vibhore.miglani@inmobi.com>
2022-01-28 20:05:04 +0530

    wip

--- scripts/azurerm_management_lock.py
+++ scripts/azurerm_management_lock.py
@@ -1,12 +1,16 @@
 import uuid
 
+from utils.utils import get_tf_config_file_path, get_logger, get_tf_compatible_name
+
+logger = get_logger(logger_name='management_lock')
+
 
 def azurerm_management_lock(crf, cde, crg, headers, requests, sub, json, az2tfmess, cldurl):
     # management locks
 
-    terraform_resource_name = "azurerm_management_lock"
+    resource_type = "azurerm_management_lock"
     azr = ""
-    if crf in terraform_resource_name:
+    if crf in resource_type:
         # REST
         # # print "REST VNets"
         url = "https://" + cldurl + "/subscriptions/" + sub + "/providers/Microsoft.Authorization/locks"
@@ -14,11 +18,11 @@ def azurerm_management_lock(crf, cde, crg, headers, requests, sub, json, az2tfme
         r = requests.get(url, headers=headers, params=params)
         azr = r.json()["value"]
 
-        tfrmf = "002-" + terraform_resource_name + "-staterm.sh"
-        tfimf = "002-" + terraform_resource_name + "-stateimp.sh"
+        tfrmf = "002-" + resource_type + "-staterm.sh"
+        tfimf = "002-" + resource_type + "-stateimp.sh"
         tfrm = open(tfrmf, 'a')
         tfim = open(tfimf, 'a')
-        print("# " + terraform_resource_name, )
+        print("# " + resource_type, )
         count = len(azr)
         print(count)
         for j in range(0, count):
@@ -29,54 +33,47 @@ def azurerm_management_lock(crf, cde, crg, headers, requests, sub, json, az2tfme
 
             # loc=azr[j]["location"]
             id = azr[j]["id"]
-            rg = id.split("/")[4].replace(".", "-").lower()
-            if rg[0].isdigit(): rg = "rg_" + rg
-            rgs = id.split("/")[4]
+            rg_orginal = id.split("/")[4]
+            if crg is not None:  # filter by resource_group
+                if rg_orginal.lower() != crg.lower():
+                    continue
+
+            tf_rg = rg_orginal.replace(".", "-").lower()
+            if tf_rg[0].isdigit():
+                tf_rg = "rg_" + tf_rg
 
             level = azr[j]["properties"]["level"]
 
+            # get scope and tf scope name
             scope1 = id.split("/Microsoft.Authorization")[0].rstrip("providers")
-
             scope = scope1.rstrip("/")
             sc = len(scope.split("/"))
-            # print sc
-            sn = scope.split("/")[sc - 1].replace(" ", "-").lower()
-            sn = sn.replace(".", "-")
-
+            logger.debug("for management lock name: {}, value of scope is {}".format(name, scope))
+            tf_scope = scope.split("/")[sc - 1].replace(" ", "-").lower()  # tf_scope : terraform scope name ?
+            tf_scope = tf_scope.replace(".", "-")
             # scope=scope.encode('utf-8', 'ignore')
-            # sn=sn.encode('utf-8', 'ignore')
+            # tf_scope=tf_scope.encode('utf-8', 'ignore')
 
-            if crg is not None:
-                if rgs.lower() != crg.lower():
-                    continue  # back to for
+            tf_name = get_tf_compatible_name(name)
+
             if cde:
                 print(json.dumps(azr[j], indent=4, separators=(',', ': ')))
 
-            rname = name.replace(".", "-")
-            rname = rname.replace("[", "-")
-            rname = rname.replace("]", "-")
-            rname = rname.replace(" ", "_")
-            # try:
-            #    rname=rname.encode('utf-8', 'ignore')
-            # except UnicodeDecodeError:
-            #    print('Problem with the name of this item: '+name)
-            #    print('Please rename this item in the Azure Portal')
-            #    rname=str(uuid.uuid4())
-            #    #rname=rname.encode('utf-8', 'ignore')
-
+            # compute tf complete name & handle scope
             try:
-                prefix = terraform_resource_name + "." + rg + '__' + rname + '__' + sn
+                tf_resource_name = tf_rg + '__' + tf_name + '__' + tf_scope
             except UnicodeDecodeError:
                 print('Problem with the scope name: ' + scope)
                 print('Please rename this item in the Azure Portal')
-                sn = str(uuid.uuid4())
-                # sn=sn.encode('utf-8', 'ignore')
-                prefix = terraform_resource_name + "." + rg + '__' + rname + '__' + sn
+                tf_scope = str(uuid.uuid4())
+                # tf_scope=tf_scope.encode('utf-8', 'ignore')
+                tf_resource_name = tf_rg + '__' + tf_name + '__' + tf_scope
+
             # prefix=tfp+"."+rg+'__'+rname
 
-            rfilename = prefix + ".tf"
+            rfilename = get_tf_config_file_path(resource_type, tf_rg, tf_name, tf_scope)
             fr = open(rfilename, 'w')
-            fr.write('resource ' + terraform_resource_name + ' "' + rg + '__' + rname + '__' + sn + '" {\n')
+            fr.write('resource ' + resource_type + ' "' + tf_resource_name + '" {\n')
             fr.write('\t name = "' + name + '"\n')
             fr.write('\t lock_level = "' + level + '"\n')
 
@@ -123,9 +120,9 @@ def azurerm_management_lock(crf, cde, crg, headers, requests, sub, json, az2tfme
                 with open(rfilename) as f:
                     print(f.read())
 
-            tfrm.write('terraform state rm ' + terraform_resource_name + '.' + rg + '__' + rname + '__' + sn + '\n')
+            tfrm.write('terraform state rm ' + resource_type + '.' + tf_resource_name + '\n')
 
-            tfcomm = 'terraform import ' + terraform_resource_name + '.' + rg + '__' + rname + '__' + sn + ' "' + id + '"\n'
+            tfcomm = 'terraform import ' + resource_type + '.' + tf_resource_name + ' "' + id + '"\n'
             tfim.write('echo "importing ' + str(j) + ' of ' + str(count - 1) + '"' + '\n')
             # tfcomm=tfcomm.encode('utf-8', 'ignore')
             tfim.write(tfcomm)
